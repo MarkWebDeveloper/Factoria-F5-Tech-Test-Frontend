f9dddcdca23c9d112936e0ab215087b2c97c648f
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-05 17:14:18 +0200

    header warning is adaptive

--- src/components/global/Warning.vue
+++ src/components/global/Warning.vue
@@ -22,10 +22,42 @@
     margin: 1rem;
 }
 p {
-    font-size: 1rem;
+    font-size: 1.2rem;
     font-family: "Ubuntu", sans-serif;
     font-weight: 500;
-    width: 12rem;
+    width: 50%;
     text-align: left;
+}
+@media only screen and (min-width: 960px) {
+    .container {
+    justify-content: space-between;
+    width: 100%;
+    box-shadow: 3px 3px 8px #888888;
+}
+.factoria-logo {
+    width: 10rem;
+    margin: 1.5rem 0rem 1.5rem 3rem;
+}
+p {
+    font-size: 1.5rem;
+    width: 70%;
+    margin: 1.5rem;
+    text-align: center;
+}
+}
+@media only screen and (min-width: 1264px) {
+    .container {
+    box-shadow: 4px 4px 8px #888888;
+}
+.factoria-logo {
+    width: 10rem;
+    margin: 1.5rem 0rem 1.5rem 3rem;
+}
+p {
+    font-size: 2rem;
+    width: 70%;
+    margin: 2rem;
+    text-align: right;
+}
 }
 </style>
