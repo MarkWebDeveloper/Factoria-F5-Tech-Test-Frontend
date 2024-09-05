312c4256189748d62c5075d5165995ba4304733f
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-05 17:27:18 +0200

    non responsive login button is ready

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,33 @@
 
 <template>
   <main>
-    
+    <div id="login-container">
+      <h2 id="greeting">Log in to see your Photos</h2>
+      <button class="button" id="login-button">LOGIN</button>
+    </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+#login-container {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+#greeting {
+  font-size: 1.1rem;
+  font-family: "Anybody", sans-serif;
+}
+#login-button {
+  background-color: $factoria-orange;
+  width: 6rem;
+  font-size: 1rem;
+  padding: 0.7rem;
+  font-family: "Ubuntu", sans-serif;
+  color: white;
+  border-radius: 10px;
+  box-shadow: 2px 2px 4px #888888;
+}
+</style>
