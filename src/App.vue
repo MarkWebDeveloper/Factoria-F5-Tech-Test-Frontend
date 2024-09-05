10e562efc09e5461c979910b74f817f34d1b98cb
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-05 16:43:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink class="link" to="/">Home</RouterLink>
+        <RouterLink class="link" to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+.link {
+  margin-left: 1rem;
+}
+</style>
